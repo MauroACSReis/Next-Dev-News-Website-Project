976cfb30597d31b16aa3ce460ddb2c651103d9cc
MauroACSReis <mauroacsreis@gmail.com>
2022-08-09 19:16:54 -0300

    First Commit

---
+++ components/Meta.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+const Meta = ({ title, keywords, description }) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>{title}</title>
+    </Head>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Crypto Newz',
+  keywords: 'web development, programming, crypto',
+  description: 'Get the latest news in web crypto dev'
+}
+
+export default Meta
